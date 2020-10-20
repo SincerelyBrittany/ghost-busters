ced5ad447e1c9cff3427d0131bba4489ea21020f
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-10-20 11:03:52 -0700

    Refactor
    Followed video tutorial for refactoring

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,17 @@
+import 'phaser';
+import logoImg from "../assets/logo.png";
+
+export default class GameScene extends Phaser.Scene{
+    constructor() {
+        //scene of game
+        super('Game');
+    }
+
+    preload(){
+        this.load.image("logo", logoImg);
+    }
+
+    create() {
+        const logo = this.add.image(400, 150, "logo");
+    }
+}
