8d11eed1845beff8d853dbe06f17e4919f53bf4b
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 13:11:54 -0400

    adds welcome to my game text to confirm routing

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,7 +3,6 @@ import logoImg from "../assets/logo.png";
 
 export default class GameScene extends Phaser.Scene{
     constructor() {
-        //scene of game
         super('Game');
     }
 
@@ -13,6 +12,7 @@ export default class GameScene extends Phaser.Scene{
 
     create() {
         const logo = this.add.image(400, 150, "logo");
+        var text = this.add.text(100,100, 'Welcome To My Game!');
     }
 
     update() {
