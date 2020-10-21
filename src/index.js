81ffbee4e3f84cd79fc75419321ee08fec39d650
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 13:10:00 -0400

    creates new maain, boot, and game scenes and adds them

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import BootScene from './scenes/BootScene';
 import MainScene from './scenes/MainScene';
 import GameScene from './scenes/GameScene';
 
+let bootScene = new BootScene();
+let mainScene = new MainScene();
+let gameScene = new GameScene();
 
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
-    this.scene.add('Boot', BootScene);
+    this.scene.add('Boot', bootScene);
+    this.scene.add("Game", gameScene);
+    this.scene.add("Main", mainScene);
     this.scene.start('Boot');
   }
 }
