51ea3dec9b12fcbf1c181aeca58ff9499a31e055
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-21 13:11:19 -0400

    adds click event that switches to the game upon click and adds single player and multiplayer text

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -11,8 +11,19 @@ export default class SceneMain extends Phaser.Scene {
 
     }
 
-    update() {
-        const ghost = this.add.image(400, 150, "ghost")
+    // update() {
+    //     const ghost = this.add.image(400, 150, "ghost")
+    // }
 
+    clickButton() {
+        this.scene.switch('Game');
+    }
+
+    create() {
+        const candle = this.add.image(500, 150, "candle");
+        var text = this.add.text(100,100, 'Single Player Game');
+        text.setInteractive({ useHandCursor: true });
+        text.on('pointerdown', () => this.clickButton());
+        var text = this.add.text(100,150, 'Multiplayer Game - Coming Soon');
     }
 }
