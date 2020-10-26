c995a16cd1e13f2de6cc6b8a6e7908fb74fb6d03
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-26 12:46:38 -0400

    adds the multiplayer scene to index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import config from './config/config';
 import BootScene from './scenes/BootScene';
 import MainScene from './scenes/MainScene';
 import GameScene from './scenes/GameScene';
+import MultiPlayerGameScene from './scenes/MultiPlayerGameScene';
 
 let bootScene = new BootScene();
 let mainScene = new MainScene();
 let gameScene = new GameScene();
+let multiPlayerGameScene = new MultiPlayerGameScene();
 
 
 class Game extends Phaser.Game {
@@ -15,6 +17,7 @@ class Game extends Phaser.Game {
     this.scene.add('Boot', bootScene);
     this.scene.add("Game", gameScene);
     this.scene.add("Main", mainScene);
+    this.scene.add("MultiPlayerGame", multiPlayerGameScene);
     this.scene.start('Boot');
   }
 }
