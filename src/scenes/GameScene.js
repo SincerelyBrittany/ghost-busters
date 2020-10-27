aae961d24535f41f4e11c33a791efef88167e7fc
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-10-27 09:11:09 -0700

    Candle Sprites
    -Candle Sprites Appear in Random Location
    -Candle Sprites bounce around world

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -4,6 +4,7 @@ import candleImg from "../assets/candle.png";
 import ghostImg from "../assets/ghost.png";
 
 
+
 export default class GameScene extends Phaser.Scene{
     constructor() {
         super('Game');
@@ -15,9 +16,29 @@ export default class GameScene extends Phaser.Scene{
     }
 
     create() {
+        //Boundaries
         this.physics.world.setBoundsCollision(true, true, true, true);
-        this.logo = this.physics.add.image(200, 300, 'logo').setCollideWorldBounds(true).setBounce(.5);
+        //Manual Boundaries
+        //this.physics.world.setBounds(0, 0, 800, 600);
 
+        this.spriteBounds = Phaser.Geom.Rectangle.Inflate(Phaser.Geom.Rectangle.Clone(this.physics.world.bounds), -100, -100);
+
+        for (var i = 0; i < 10; i++){
+            this.pos = Phaser.Geom.Rectangle.Random(this.spriteBounds);
+            this.block = this.physics.add.sprite(this.pos.x, this.pos.y, 'candle');
+
+            //velocity setter
+            this.block.setVelocity(Phaser.Math.Between(200, 300), Phaser.Math.Between(200, 400));
+            this.block.setBounce(1).setCollideWorldBounds(true);
+            if (Math.random() > 0.5){
+                this.block.body.velocity.x *= -1;
+            }
+            else {
+                this.block.body.velocity.y *= -1;
+            }
+        }
+        
+        
         //time for game
         this.initialTime = 30;
         this.text = this.add.text(32,32, 'Time Remaining: ' + this.formatTime(this.initialTime));
